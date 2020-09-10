f20a4bc73c765b55a2c03940ed30d8842cdf3145
UDQD <70804333+UDQD@users.noreply.github.com>
2020-09-10 15:44:14 +0300

    Теперь норм

--- pip_list/main.py
+++ pip_list/main.py
@@ -2,9 +2,8 @@ from pip._vendor import pkg_resources
 
 
 def srs(t):
-    out = ''
     try:
-        _package = pkg_resources.working_set.by_key[t.replace('-','_')]
+        _package = pkg_resources.working_set.by_key[t.replace('-', '_')]
 
         for el in _package.requires():
             eln = str(el)
@@ -12,14 +11,14 @@ def srs(t):
                 eln = eln[:eln.index('=') - 1]
             except ValueError:
                 pass
-            if '-' in eln:
-                eln = eln.replace('-','_')
+
+            eln = eln.replace('-', '_')
             print(str(t) + '->' + "\"" + eln + "\"")
             srs(eln)
 
-        return out
     except KeyError:
         pass
+
 
 pack = input(':')
 print('digraph G {')
